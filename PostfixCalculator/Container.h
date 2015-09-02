61d3c02bad846a1294e898ea8685f1cc45ded4b2
Александр Петренко <gafk555@gmail.com>
2015-09-02 20:46:57 +0300

    Настроена форма главного окна и добавлены методы в классе Container.

--- PostfixCalculator/Container.h
+++ PostfixCalculator/Container.h
@@ -3,13 +3,26 @@
 
 #include "Element.h"
 
-template <class T>
+/*!
+ * Класс стека на основе односвязного списка
+ */
+template <typename T>
 class Container
 {
 public:
 	Container();
 	~Container();
+
+	/* Поместить элемент на вершину стека */
+	void push(T value);
+	/* Удалить элемент с вершины стека */
+	Element<T> pop();
+	/* Проверить, пуст ли стек */
+	bool isEmpty();
+
 private:
+	Element<T> *top;			// Указатель на вершину стека
+	unsigned int size;			// Размер стека
 };
 
 #endif // CONTAINER_H
--- PostfixCalculator/GeneratedFiles/ui_postfixcalculator.h
+++ PostfixCalculator/GeneratedFiles/ui_postfixcalculator.h
@@ -14,10 +14,11 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenuBar>
-#include <QtWidgets/QStatusBar>
-#include <QtWidgets/QToolBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QTextEdit>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -25,28 +26,69 @@ QT_BEGIN_NAMESPACE
 class Ui_PostfixCalculatorClass
 {
 public:
-    QMenuBar *menuBar;
-    QToolBar *mainToolBar;
     QWidget *centralWidget;
-    QStatusBar *statusBar;
+    QLineEdit *lineEdit;
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
+    QPushButton *pushButton;
+    QLabel *label_4;
+    QTextEdit *textEdit;
 
     void setupUi(QMainWindow *PostfixCalculatorClass)
     {
         if (PostfixCalculatorClass->objectName().isEmpty())
             PostfixCalculatorClass->setObjectName(QStringLiteral("PostfixCalculatorClass"));
         PostfixCalculatorClass->resize(600, 400);
-        menuBar = new QMenuBar(PostfixCalculatorClass);
-        menuBar->setObjectName(QStringLiteral("menuBar"));
-        PostfixCalculatorClass->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(PostfixCalculatorClass);
-        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        PostfixCalculatorClass->addToolBar(mainToolBar);
+        PostfixCalculatorClass->setMinimumSize(QSize(600, 400));
+        PostfixCalculatorClass->setMaximumSize(QSize(600, 400));
         centralWidget = new QWidget(PostfixCalculatorClass);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        lineEdit = new QLineEdit(centralWidget);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+        lineEdit->setGeometry(QRect(20, 100, 231, 20));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(20, 10, 561, 51));
+        QPalette palette;
+        QBrush brush(QColor(85, 85, 127, 255));
+        brush.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::WindowText, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::WindowText, brush);
+        QBrush brush1(QColor(120, 120, 120, 255));
+        brush1.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Disabled, QPalette::WindowText, brush1);
+        label->setPalette(palette);
+        QFont font;
+        font.setFamily(QStringLiteral("Yu Mincho Light"));
+        font.setPointSize(12);
+        font.setBold(true);
+        font.setWeight(75);
+        label->setFont(font);
+        label->setAlignment(Qt::AlignCenter);
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(20, 70, 191, 16));
+        QFont font1;
+        font1.setFamily(QStringLiteral("Yu Gothic UI Semilight"));
+        font1.setPointSize(12);
+        label_2->setFont(font1);
+        label_3 = new QLabel(centralWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(20, 210, 231, 51));
+        label_3->setFont(font1);
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(150, 360, 291, 31));
+        pushButton->setFont(font1);
+        label_4 = new QLabel(centralWidget);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(300, 70, 191, 16));
+        label_4->setFont(font1);
+        textEdit = new QTextEdit(centralWidget);
+        textEdit->setObjectName(QStringLiteral("textEdit"));
+        textEdit->setGeometry(QRect(300, 100, 251, 241));
         PostfixCalculatorClass->setCentralWidget(centralWidget);
-        statusBar = new QStatusBar(PostfixCalculatorClass);
-        statusBar->setObjectName(QStringLiteral("statusBar"));
-        PostfixCalculatorClass->setStatusBar(statusBar);
 
         retranslateUi(PostfixCalculatorClass);
 
@@ -56,6 +98,11 @@ public:
     void retranslateUi(QMainWindow *PostfixCalculatorClass)
     {
         PostfixCalculatorClass->setWindowTitle(QApplication::translate("PostfixCalculatorClass", "PostfixCalculator", 0));
+        label->setText(QApplication::translate("PostfixCalculatorClass", "\320\237\320\276\321\201\321\202\321\204\320\270\320\272\321\201\320\275\321\213\320\271 \320\272\320\260\320\273\321\214\320\272\321\203\320\273\321\217\321\202\320\276\321\200", 0));
+        label_2->setText(QApplication::translate("PostfixCalculatorClass", "\320\241\321\202\321\200\320\276\320\272\320\260 \320\264\320\273\321\217 \320\262\321\213\321\207\320\270\321\201\320\273\320\265\320\275\320\270\321\217:", 0));
+        label_3->setText(QApplication::translate("PostfixCalculatorClass", "\320\240\320\265\320\267\321\203\320\273\321\214\321\202\320\260\321\202:", 0));
+        pushButton->setText(QApplication::translate("PostfixCalculatorClass", "\320\237\320\276\320\273\321\203\321\207\320\270\321\202\321\214 \321\200\320\265\320\267\321\203\320\273\321\214\321\202\320\260\321\202", 0));
+        label_4->setText(QApplication::translate("PostfixCalculatorClass", "\320\241\320\277\321\200\320\260\320\262\320\272\320\260:", 0));
     } // retranslateUi
 
 };
